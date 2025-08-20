4db739ea8630185ca0cc1d09b08d709cedac01e1
digitallegion <digitallegion2015@gmail.com>
2025-08-20 13:31:37 +0300

    Update server port configuration to 5001 and mark client subproject as dirty

--- server/server.js
+++ server/server.js
@@ -284,7 +284,7 @@ module.exports = app;
 
 // Start the server only if not in a Vercel environment
 if (!process.env.VERCEL) {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
